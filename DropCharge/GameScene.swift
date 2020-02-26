f3d4d415114887f68f015c38c3196456a40c49f7
Manel Matougui <m.matougui@gmail.com>
2020-02-26 13:26:28 -0800

    add bomb explosion

--- DropCharge/GameScene.swift
+++ DropCharge/GameScene.swift
@@ -424,7 +424,11 @@ class GameScene: SKScene , SKPhysicsContactDelegate{
         SKAction.run(startGame)]))
     }
         func startGame() {
-        fgNode.childNode(withName: "Bomb")!.removeFromParent()
+        let bomb = fgNode.childNode(withName: "Bomb")!
+        let bombBlast = explosion(intensity: 2.0)
+        bombBlast.position = bomb.position
+        fgNode.addChild(bombBlast)
+        bomb.removeFromParent()
         gameState = .playing
         player.physicsBody!.isDynamic = true
         boostPlayer()
@@ -491,6 +495,30 @@ class GameScene: SKScene , SKPhysicsContactDelegate{
         gameOverSprite.position = camera!.position
         gameOverSprite.zPosition = 10
         addChild(gameOverSprite)
+    }
+    
+    func explosion(intensity: CGFloat) -> SKEmitterNode {
+        let emitter = SKEmitterNode()
+        let particleTexture = SKTexture(imageNamed: "spark")
+        emitter.zPosition = 2
+        emitter.particleTexture = particleTexture
+        emitter.particleBirthRate = 4000 * intensity
+        emitter.numParticlesToEmit = Int(400 * intensity)
+        emitter.particleLifetime = 2.0
+        emitter.emissionAngle = CGFloat(90.0).degreesToRadians()
+        emitter.emissionAngleRange = CGFloat(360.0).degreesToRadians()
+        emitter.particleSpeed = 600 * intensity
+        emitter.particleSpeedRange = 1000 * intensity
+        emitter.particleAlpha = 1.0
+        emitter.particleAlphaRange = 0.25
+        emitter.particleScale = 1.2
+        emitter.particleScaleRange = 2.0
+        emitter.particleScaleSpeed = -1.5
+        emitter.particleColor = SKColor.orange
+        emitter.particleColorBlendFactor = 1
+        emitter.particleBlendMode = SKBlendMode.add
+        emitter.run(SKAction.removeFromParentAfterDelay(2.0))
+      return emitter
     }
     
     }
